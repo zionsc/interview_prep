a322076c535cca68a6cf651f7f2e8b94eca3d51b
Seeon Chang <zionchan@usc.edu>
2024-01-18 20:51:45 -0800

    parser

--- csv_parser.py
+++ csv_parser.py
@@ -1,2 +1,27 @@
 import csv
+import math
+from collections import defaultdict
 
+def parser1(file_name, dict):
+    with open(file_name, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        for line in csv_reader:
+            dict[line['NAME']].append(line['LEG_LENGTH'])
+    return dict
+
+def parser2(file_name, dict):
+    with open(file_name, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        for line in csv_reader:
+            dict[line['NAME']].append(line['STRIDE_LENGTH'])
+    return dict
+
+def main():
+    dinosaurs = defaultdict(list)
+    dinosaurs = parser1('dataset1.csv', dinosaurs)
+    dinosaurs = parser2('dataset2.csv', dinosaurs)
+    
+    
+
+if __name__ == "__main__":
+    main()
