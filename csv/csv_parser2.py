e916b5d0507a7981e45d0ebc6969a5cc83611ffe
Seeon Chang <zionchan@usc.edu>
2024-01-18 22:03:18 -0800

    csv

--- csv/csv_parser2.py
+++ csv/csv_parser2.py
@@ -1,5 +1,6 @@
 import csv
 from collections import defaultdict
+from math import sqrt
 
 def parser1(file_name, dict):
     with open(file_name, 'r') as csv_file:
@@ -17,13 +18,20 @@ def parser2(file_name, dict):
 
 
 def main():
+    g = 9.8
     dinosaurs = defaultdict(dict) # dino name : dict{stance:, leg:, thing:}
     parser1('dataset1.csv', dinosaurs)
     parser2('dataset2.csv', dinosaurs)
-    print(dinosaurs)
+    
+    dino_speed = []
+    for name,dct in dinosaurs.items():
+        if 'STRIDE_LENGTH' in dct and 'STANCE' in dct and 'LEG_LENGTH' in dct and dct['STANCE'] == "bipedal":
+            speed = ((dct['STRIDE_LENGTH'] / dct['LEG_LENGTH']) - 1) * sqrt(dct['LEG_LENGTH'] * g)
+            dino_speed.append((name, speed))
+    dino_speed.sort(key=lambda x:x[1])
 
-
-
+    res = [name for name,speed in dino_speed]
+    print(res)
 
 if __name__ == "__main__":
     main()
